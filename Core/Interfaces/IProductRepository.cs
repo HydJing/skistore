94b12ca944281daa72d0c65369bd99f7f90f4b69
Wei Jing <jaychou123103@hotmail.com>
2020-10-11 23:15:23 +1300

    add get single product and all prodcuts

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -7,6 +7,6 @@ namespace Core.Interfaces
     public interface IProductRepository
     {
          Task<Product> GetProductByIdAsync(int id);
-         Task<IReadOnlyList<Product>> GetProductsAsync(int id);
+         Task<IReadOnlyList<Product>> GetProductsAsync();
     }
 }
