32e9adb47e754aa0ffd8eb2b515b521d5e959070
Wei Jing <jaychou123103@hotmail.com>
2020-12-11 01:17:30 +1300

    add total component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule, CarouselModule } from 'ngx-bootstrap';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -17,6 +18,7 @@ import { PagerComponent } from './components/pager/pager.component';
     PagingHeaderComponent,
     PagerComponent,
     CarouselModule,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule { }
