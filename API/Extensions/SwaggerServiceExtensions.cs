e092e0abbaa615b40c3147a5eb15a301e6d64278
Wei Jing <jaychou123103@hotmail.com>
2020-12-24 03:18:03 +1300

    set up swagger config for identity

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
@@ -9,9 +10,37 @@ namespace API.Extensions
         {
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "SkiStore API", Version = "v1"});
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Schema",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement {{securitySchema, new[] {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
+                
             });
 
             return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumention(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {c
+                .SwaggerEndpoint("/swagger/v1/swagger.json", "SkiStore API v1");});
+
+            return app;
         }
     }
 }
