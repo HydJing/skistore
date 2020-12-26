0e4cd70210bdbf8bbab7ffc42fbc29fe4dc489ef
Wei Jing <jaychou123103@hotmail.com>
2020-12-27 01:46:24 +1300

    add login form

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { PaginationModule, CarouselModule } from 'ngx-bootstrap';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,13 +13,15 @@ import { OrderTotalsComponent } from './components/order-totals/order-totals.com
     CommonModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
+    ReactiveFormsModule
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
     CarouselModule,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
