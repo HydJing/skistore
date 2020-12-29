a8b9716c4fe2ef18d1c3b49c6f41ebb268e6c7b5
Wei Jing <jaychou123103@hotmail.com>
2020-12-30 02:37:01 +1300

    add NG text input

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -9,9 +9,15 @@ import { PagingHeaderComponent } from "./components/paging-header/paging-header.
 import { PagerComponent } from "./components/pager/pager.component";
 import { OrderTotalsComponent } from "./components/order-totals/order-totals.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { TextInputComponent } from "./components/text-input/text-input.component";
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
+  declarations: [
+    PagingHeaderComponent,
+    PagerComponent,
+    OrderTotalsComponent,
+    TextInputComponent,
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -27,6 +33,7 @@ import { ReactiveFormsModule } from "@angular/forms";
     OrderTotalsComponent,
     ReactiveFormsModule,
     BsDropdownModule,
+    TextInputComponent,
   ],
 })
 export class SharedModule {}
