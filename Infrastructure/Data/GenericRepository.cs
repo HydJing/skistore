cb0365ac87f0ba0d69fbf99b8c643e1febc3763c
Wei Jing <jaychou123103@hotmail.com>
2021-01-11 21:28:54 +1300

    Update generic repository

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -16,9 +16,20 @@ namespace Infrastructure.Data
             _context = context;
         }
 
-        public async Task<int> CountAsync(ISpecification<T> spec)
+        public void Add(T entity)
         {
-            return await ApplySpecification(spec).CountAsync();
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
         }
 
         public async Task<T> GetByIdAsync(int id)
@@ -41,9 +52,16 @@ namespace Infrastructure.Data
             return await ApplySpecification(spec).ToListAsync();
         }
 
+
+
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+        }
+
+        public Task<int> CountAsync(ISpecification<T> spec)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
