884c0ecdac292f6bde0ca5e3aac3cfcbd87079d1
Wei Jing <jaychou123103@hotmail.com>
2021-01-19 22:40:47 +1300

    add delivery methods service functions

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { IDeliveryMethod } from "src/app/shared/model/deliveryMethod";
+import { CheckoutService } from "../checkout.service";
 
 @Component({
-  selector: 'app-checkout-delivery',
-  templateUrl: './checkout-delivery.component.html',
-  styleUrls: ['./checkout-delivery.component.scss']
+  selector: "app-checkout-delivery",
+  templateUrl: "./checkout-delivery.component.html",
+  styleUrls: ["./checkout-delivery.component.scss"],
 })
 export class CheckoutDeliveryComponent implements OnInit {
+  @Input() checkoutForm: FormGroup;
+  deliveryMethods: IDeliveryMethod[];
 
-  constructor() { }
+  constructor(private CheckoutService: CheckoutService) {}
 
   ngOnInit() {
+    this.CheckoutService.getDeliveryMethods().subscribe(
+      (dm: IDeliveryMethod[]) => {
+        this.deliveryMethods = dm;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
